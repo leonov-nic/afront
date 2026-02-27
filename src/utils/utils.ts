b85d5fb452ebb17e7985f2bee6dc5d3fad3d7372
Nikolay <leonov-nic@yandex.ru>
2026-02-28 01:52:03 +0300

    fix load file feb6

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -309,18 +309,18 @@ export const createRowsForExellFile = async (jobs: TJobRDO[]) => {
     return {
     // "Date": new Date(job.createdAt).toLocaleString('ru-RU', {year: 'numeric', month: '2-digit', day: '2-digit'}),
     "Date": new Date(job.createdAt),
-    "№": job.employee.registrationNumber,
+    "№": job.employee?.registrationNumber || "",
     "Type Of Job&Detail": `${job.typeOfJob}${job.detail?.shortName !== '0' ? job.detail?.shortName : ''}`,
     "Quantity": job.quantity !== 0 ? Number(job.quantity) : '',
     "TotalHours": job.totalHours === '-' ? "" : (job.totalHours || ""),
     "Extra": job.extra || "",
     "Comment": job.comment || "",
     "Type Of Job": job.typeOfJob,
-    "Employee": job.employee.familyName,
+    "Employee": job.employee?.familyName || "no name",
     "Detail": job.detail?.shortName !== '0' ? job.detail?.shortName : '',
     "TimeFrom": job.timeFrom !== '-' ? getHoursAndSeconds(job.timeFrom) : '',
     "TimeTo": job.timeTo !== '-' ? getHoursAndSeconds(job.timeTo) : '',
-    "Master": job.master.name,
+    "Master": job.master?.name || "no master",
     "isLunch": job.isLunch ? 'yes' : 'NO'};
   });
 
